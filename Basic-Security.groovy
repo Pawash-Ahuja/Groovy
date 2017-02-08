ef5eb1ffbd9dc799ee56bf50ba41ef42095f3427
Pawash Ahuja <pahuja2@ncsu.edu>
2017-02-08 18:44:24 -0500

    Update Basic-Security.groovy

--- Basic-Security.groovy
+++ Basic-Security.groovy
@@ -3,7 +3,13 @@
 import jenkins.model.*
 import hudson.util.*;
 import jenkins.install.*;
+import hudson.security.*
+  
+def instance = Jenkins.getInstance();
+def hudsonRealm = new HudsonPrivateSecurityRealm(false);
+  
+instance.setInstallState(InstallState.INITIAL_SETUP_COMPLETED);
 
-def instance = Jenkins.getInstance()
-
-instance.setInstallState(InstallState.INITIAL_SETUP_COMPLETED)
+hudsonRealm.createAccount("pawash","ahuja");
+instance.setSecurityRealm(hudsonRealm);
+instance.save();
